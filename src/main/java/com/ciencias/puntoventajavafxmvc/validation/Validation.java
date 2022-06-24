f1d4e03b41080fc564996007ea0136aad7e6c891
Brian Murillo <bmurillo1800@innovaccion.mx>
2022-06-24 13:00:18 -0500

    method validateInputNumbre added and validation the phone in controller completed

--- src/main/java/com/ciencias/puntoventajavafxmvc/validation/Validation.java
+++ src/main/java/com/ciencias/puntoventajavafxmvc/validation/Validation.java
@@ -6,10 +6,18 @@ import javafx.scene.input.KeyEvent;
 public class Validation {
 
     public static void validateInputText(TextField text){
-
         text.addEventFilter(KeyEvent.KEY_TYPED, event -> {
             char input = event.getCharacter().charAt(0);
             if(!Character.isLetter(input)){
+                event.consume();
+            }
+        });
+    }
+
+    public static void validateInputNumber(TextField text){
+        text.addEventFilter(KeyEvent.KEY_TYPED, event ->{
+            char input = event.getCharacter().charAt(0);
+            if(!Character.isDigit(input)){
                 event.consume();
             }
         });
